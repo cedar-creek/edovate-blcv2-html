abd07adede8f9ba614d20588c2fbbdd883a83d3a
Simon <simon@risberg.eu>
2022-11-03 01:48:06 +0100

    Add orders & shipping and parent dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  mode: "jit",
+  darkMode: "class",
+  theme: {
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.5rem" }],
+      base: ["1rem", { lineHeight: "1.75rem" }],
+      lg: ["1.125rem", { lineHeight: "2rem" }],
+      xl: ["1.25rem", { lineHeight: "2rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["2rem", { lineHeight: "2.5rem" }],
+      "4xl": ["2.5rem", { lineHeight: "3.5rem" }],
+      "5xl": ["3rem", { lineHeight: "3.5rem" }],
+      "6xl": ["3.75rem", { lineHeight: "1" }],
+      "7xl": ["4.5rem", { lineHeight: "1.1" }],
+      "8xl": ["6rem", { lineHeight: "1" }],
+      "9xl": ["8rem", { lineHeight: "1" }],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        display: ["Lexend", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        bg: "#F5F8FA",
+        primary: {
+          400: "#48B2E2",
+          500: "#2873C9",
+          600: "#0A316A",
+          700: "#0A2041",
+          800: "#3F83F8",
+          900: "#1C64F2",
+        },
+        gray: {
+          550: "#EFF0F3",
+        },
+      },
+
+      maxWidth: {
+        "3.5xl": "50rem",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
